849dc8c6edffc030eff11e1cb22a3ce04f2dbb02
Igor Kesler <i.kesler@live.com>
2014-06-13 17:01:45 +0400

    Change routing and fix plugin loading

--- WebApi/Controllers/WeatherController.cs
+++ WebApi/Controllers/WeatherController.cs
@@ -12,11 +12,13 @@ namespace WeatherAggregator.WebApi.Controllers
     {
         private readonly CoreFacade _facade = new CoreFacade();
 
-        public List<SourceModel> GetSources()
+        [HttpGet]
+        public List<SourceModel> Sources()
         {
             return _facade.GetSources().Select(SourceModel.Map).ToList();
         }
 
+        [HttpPost]
         public List<Weather> GetWeather(List<Guid> sources, DateRange dateRange, Location location)
         {
             return _facade.GetWeather(sources, dateRange, location);
