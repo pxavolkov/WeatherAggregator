1ad1644683473f3b4eaaba0c79c4c5ed70585b07
pxavolkov <a.volkov@svitla.com>
2014-06-13 16:58:09 +0400

    Initial implementation of Forecast.io source adapter

---
+++ Sources/ForecastioSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using WeatherAggregator.Core.Entities;
+using WeatherAggregator.Core.Interfaces;
+
+namespace Sources
+{
+    public class ForecastioSource:ISource
+    {
+        public Guid Id { get { return new Guid("2D498C9F-8294-4A47-97E4-6CF1B1800F3E"); } }
+        public string Name { get { return "Forecast.io source";  } }
+        public string Snippet { get { return "<a href='http://forecast.io/'>Powered by Forecast</a>"; } }
+        public List<Weather> GetWeather(DateRange dateRange, Location location)
+        {
+            var result = new List<Weather>();
+            var day = dateRange.From;
+            while (day < dateRange.To)
+            {
+                var w = new Weather{Date = day, Temperature = 15, Cloudness = 30};
+                result.Add(w);
+                day = day.AddDays(1);
+            }
+
+            return result;
+        }
+    }
+}
