ec0e55953ec0a5311f81f89305afba5573b08c12
atanya <t.s.arkhipova@gmail.com>
2014-06-16 15:13:55 +0400

    ClientSide: add Angularjs

---
+++ ClientSideWeb/Scripts/weather.js
@@ -0,0 +1,32 @@
+﻿window.weatherAggregator = {
+    init: function () {
+
+        weatherAggregator.proxy.getSources(weatherAggregator.onGetSources);
+    },
+
+    onGetSources: function() {
+        var weatherApp = angular.module('weatherApp', []);
+
+        weatherApp.controller('WeatherController', function ($scope) {
+            $scope.sources = [
+              {
+                  Id: "123",
+                  Name: 'GisMeteo'
+              },
+              {
+                  Id: "1234",
+                  Name: 'Site1'
+              },
+              {
+                  Id: "12345",
+                  Name: 'Site2'
+              }
+            ];
+
+            $scope.getWeather = function() {
+                alert("The weather is good!");
+            };
+        });
+    }
+};
+$(weatherAggregator.init());
---
+++ ClientSideWeb/Scripts/weather.proxy.js
@@ -0,0 +1,29 @@
+﻿weatherAggregator.proxy = function () {
+
+    var sendAjaxRequest = function(type, url, data, onSuccess) {
+
+        var jsonRequest = JSON.stringify(data);
+
+        $.ajax({
+            url: url,
+            type: type,
+            data: jsonRequest,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: onSuccess,
+            error: function (xhr) {
+                alert(xhr.status);
+            }
+        });
+    };
+
+    return {
+        getSources: function (onSuccess) {
+            sendAjaxRequest("Get", "/Weather/Sources", onSuccess);
+        },
+
+        getWeather: function (onSuccess) {
+            sendAjaxRequest("POST", "", onSuccess);
+        }
+    };
+}();
