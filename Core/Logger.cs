f8f3411e6704320d95a889a444595e2e2701fff8
Igor Kesler <i.kesler@live.com>
2014-07-08 18:07:30 +0400

    Switch logging to NLog

--- Core/Logger.cs
+++ Core/Logger.cs
@@ -1,29 +1,38 @@
 ﻿using System;
 using System.Linq;
-using log4net;
 using Newtonsoft.Json;
+using NLog;
+using NLog.Common;
 
 namespace WeatherAggregator.Core
 {
     static class Logger
     {
+        private const string LoggerName = "MovieServiceLogger";
+
+        private static readonly NLog.Logger _nLog = LogManager.GetLogger(LoggerName);
+
         static Logger()
         {
-            log4net.Config.XmlConfigurator.Configure();
-            _log4net = LogManager.GetLogger(typeof(object));
-        }
+            // enable internal logging to the console
+            InternalLogger.LogToConsole = true;
 
-        private static readonly ILog _log4net;
+            // enable internal logging to a file
+            InternalLogger.LogFile = "c:\\log.txt";
+
+            // set internal log level
+            InternalLogger.LogLevel = LogLevel.Trace;
+        }
 
         public static void Info(string format, params object[] @params)
         {
             var jsonParams = @params.Select(p => JsonConvert.SerializeObject(p));
-            _log4net.InfoFormat(format, (object[]) jsonParams.ToArray());
+            _nLog.Info(format, (object[]) jsonParams.ToArray());
         }
 
         public static void Error(Exception e)
         {
-            _log4net.Error("Unhandled exception", e);
+            _nLog.Error("Unhandled exception", e);
         }
     }
 }
